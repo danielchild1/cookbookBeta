a560dec872d868d4ef814b376dc7112e85a7e38a
Daniel Child <danielchild1@mail.weber.edu>
2020-04-17 13:45:35 -0600

    finaly some progress

--- ng/src/app/app-routing.module.ts
+++ ng/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { RecipeFourmComponent } from './components/recipe-fourm/recipe-fourm.component';
+import { AuthGuard } from './services/auth.guard';
+import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
+import { UserRegisterComponent } from './components/user-register/user-register.component';
+import { UserSigninComponent } from './components/user-signin/user-signin.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "recipes", pathMatch: "full"},
+  {path: 'recipes', component: RecipeListComponent},
+  {path: 'recipes/new', component: RecipeFourmComponent, canActivate: [AuthGuard]},
+  {path: 'recipes/:id', component: RecipeDetailComponent},
+  {path: 'recipes/:id/edit', component: RecipeFourmComponent, canActivate: [AuthGuard]},
+  {path: 'register', component: UserRegisterComponent},
+  {path: 'signin', component: UserSigninComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
